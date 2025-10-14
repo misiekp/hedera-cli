cee7a74a36fd3133aef6610ce08711a96427ea1b
michalrozekariane <michal.rozek@arianelabs.com>
2025-10-14 12:43:05 +0200

    feat: remove message abount setting envs for credentials
    Signed-off-by: michalrozekariane <michal.rozek@arianelabs.com>

--- src/plugins/credentials/commands/list.ts
+++ src/plugins/credentials/commands/list.ts
@@ -15,9 +15,6 @@ export function listHandler(args: CommandHandlerArgs): Promise<void> {
     if (credentials.length === 0) {
       logger.log('   No credentials stored');
       logger.log('   Use "credentials set" to add credentials');
-      logger.log(
-        '   Or set HEDERA_ACCOUNT_ID and HEDERA_PRIVATE_KEY environment variables',
-      );
     } else {
       credentials.forEach((cred, index) => {
         logger.log(`   ${index + 1}. Key Reference ID: ${cred.keyRefId}`);
